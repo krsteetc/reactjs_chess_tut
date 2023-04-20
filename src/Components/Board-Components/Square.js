4959e89e2ec0565694b0deec1b9715fe75fc438b
damjan <joveski.damjan@uklo.edu.mk>
2023-04-20 11:19:04 +0200

    to do

--- src/Components/Board-Components/Square.js
+++ src/Components/Board-Components/Square.js
@@ -5,16 +5,21 @@ function Square (props) {
 
     const isDark = props.isDark;
 
-    const piece = props.positions.find(
-        (p) => p.x === parseInt(props.x) && p.y === parseInt(props.y)
-    );
-    const isEmpty = !piece || piece.type === 'empty';
+    // const piece = props.positions.find(
+    //     (p) => p.x === parseInt(props.x) && p.y === parseInt(props.y)
+    // );
+    // const isEmpty = !piece || piece.type === 'empty';
+    const isEmpty = props.type === 'empty';
 
     return (
         <div className={`Square ${isDark ? 'Dark' : 'Light'}`}>
-            {!isEmpty && <Piece color={piece.color} type={piece.type} />}
+            {!isEmpty && <Piece color={props.color} type={piece.type} />}
         </div>
     );
 }
 
 export default Square;
+
+// To-do :
+// unificirana nomenklatura isDark => isDarkSquare
+// type so color vo edno
