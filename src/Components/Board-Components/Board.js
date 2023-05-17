d5b3982e73dc7620a0ffb82bc7ef1de31cc1b84f
Filip Tomevski <ftomevski16@gmail.com>
2023-05-17 23:27:22 +0200

    Figurite se dvizhat (bez logika) i x, y koordinatite se korektirani

--- src/Components/Board-Components/Board.js
+++ src/Components/Board-Components/Board.js
@@ -1,85 +1,58 @@
-import { useRef } from 'react';
 import './Board.css'
 import Square from "./Square";
+import {useState} from "react";
 
 function Board (props) {
-    
-    const chessboardRef = useRef(null);
-    let activePiece = null;
-
-        function grabPiece(e) {
-            const element = e.target;
-            const x = e.clientX - 50;
-            const y = e.clientY -50;
-            if(element.classList.contains('Piece')) {
-                element.style.position = "absolute";
-                element.style.left = `${x}px`
-                element.style.top = `${y}px`;
-                activePiece = element;
-            }
-        }
-        function movePiece(e) {
-            const chessboard = chessboardRef.current;
-            if(activePiece && chessboard) {
-                const minX = chessboard.offsetLeft - 25;
-                const minY = chessboard.offsetLeft -540;
-                const maxX = chessboard.offsetLeft + chessboard.clientWidth - 80;
-                const maxY = chessboard.offsetLeft + chessboard.clientHeight - 580;
-                const x = e.clientX - 50;
-                const y = e.clientY - 50;
-                activePiece.style.position = "absolute";
-                console.log(y)
-                // activePiece.style.left = `${x}px`;
-                activePiece.style.top = `${y}px`;
-                if(x < minX) {
-                    activePiece.style.left = `${minX}px`;
-                } else if(x > maxX) {
-                    activePiece.style.left = `${maxX}px`
-                } else {
-                    activePiece.style.left = `${x}px`
-                }
-
-                if(y < minY) {
-                    activePiece.style.top = `${minY}px`;
-                } else if(y > maxY) {
-                    activePiece.style.top = `${maxY}px`;
-                }
-                else {
-                    activePiece.style.top = `${y}px`;
-                }
-
-            }
-        }
-        function dropPiece(e) {
-            activePiece = null;
-        //     const chessboard = chessboardRef.current;
-        //     if (activePiece && chessboard) {
-        //       const x = Math.floor((e.clientX - chessboard.offsetLeft) / 100);
-        //       const y = Math.floor((e.clientY - chessboard.offsetTop) / 100)
-        // }
-    }
 
 
     const verticalAxis = ['8','7','6','5','4','3','2','1'] ;
     const horizontalAxis = ['1','2','3','4','5','6','7','8'];
 
-    return (
-        <div onMouseMove={e => movePiece(e)}
-        onMouseDown={e => grabPiece(e)}
-        onMouseUp={e => dropPiece(e)}
-        className="board-card"
-        ref = {chessboardRef}
-        >
+    const [selectedPiece, setSelectedPiece] = useState(null);
 
+    const [isPieceSelected,setIsPieceSelected] = useState(false);
+
+    const [positions, setPositions] = useState(props.positions)
+
+    function selectPiece(piece){
+        setSelectedPiece(piece)
+    }
+
+    function isSelected (isTrue) {
+        setIsPieceSelected(isTrue)
+    }
+
+    function movePiece(piece, x, y) {
+        const moveTo = positions.findIndex(
+            (obj) => obj.x === parseInt(x) && obj.y === parseInt(y)
+        );
+        const removeFrom = positions.findIndex(
+            (obj) => obj.x === parseInt(piece.x) && obj.y === parseInt(piece.y)
+        );
+        const updatedPositions = [...positions];
+        updatedPositions[removeFrom] = { ...updatedPositions[removeFrom], type: "empty" };
+        updatedPositions[moveTo] = { ...updatedPositions[moveTo], type: piece.type };
+        setPositions(updatedPositions);
+    }
+
+
+
+    return (
+        <div className="board-card">
 
             {horizontalAxis.map((letter) =>
                 verticalAxis.map((number) =>
                     <Square
-                    x={letter}
-                    y={number}
+                    x={number}
+                    y={letter}
                     isSquareDark={props.isSquareDark(letter, number)}
-                    key={`${letter},${number}`}
-                    positions={props.positions}
+                    key={`${letter}-${number}`}
+                    positions={positions}
+                    onSelectPiece={selectPiece}
+                    onIsPieceSelected={isSelected}
+                    selectedPiece={selectedPiece}
+                    isPieceSelected={isPieceSelected}
+                    onMovePiece={movePiece}
                     />
                 )
             )}
--- src/Components/Board-Components/Piece.js
+++ src/Components/Board-Components/Piece.js
@@ -3,11 +3,10 @@ import './Piece.css';
 function Piece (props) {
 
     const type = props.type;
-    const pieceImage = `/piece-images/${type}.png`
 
     return (
-        <div className='Piece' style={{backgroundImage: `url(${pieceImage})`}}>
-            {/* <img src={`/piece-images/${type}.png`} alt={`${type}`}/> */}
+        <div className='Piece'>
+            <img src={`/piece-images/${type}.png`} alt={`${type}`}/>
         </div>
             )
 }
