81a433894a1cff1493a44b8060eaf4f653feb9ff
damjan <joveski.damjan@uklo.edu.mk>
2023-06-08 12:57:02 +0200

    Legal Moves for rooks added and some slight fixes for pawns

--- src/Components/Board-Components/Board.js
+++ src/Components/Board-Components/Board.js
@@ -49,9 +49,15 @@ function Board (props) {
             case 'pawn':
                 if (color === 'white') {
                     if (piece.y === 2) {
+                        let endFlag = true;
                         for (let i = 1; i <= 2; i++) {
-                            const setLegal = props.squares.findIndex(obj => obj.x === piece.x && obj.y === i + piece.y && obj.isEmpty === true);
-                            updatedLegalMoves[setLegal] = { ...updatedLegalMoves[setLegal], isLegal: true };
+                            const setLegal = props.squares.findIndex(obj => obj.x === piece.x && obj.y === i + piece.y);
+                            if (props.squares[setLegal].isEmpty){
+                                updatedLegalMoves[setLegal] = { ...updatedLegalMoves[setLegal], isLegal: true };
+                                endFlag = false;
+                            }
+                            if (endFlag === true)
+                                break;
                         }
                     } else {
                         const setLegal = props.squares.findIndex(obj => obj.x === piece.x && obj.y === piece.y + 1 && obj.isEmpty === true);
@@ -69,9 +75,15 @@ function Board (props) {
                     }
                 } else {
                     if (piece.y === 7) {
+                        let endFlag = true;
                         for (let i = 1; i <= 2; i++) {
-                            const setLegal = props.squares.findIndex(obj => obj.x === piece.x && obj.y === piece.y - i && obj.isEmpty === true);
-                            updatedLegalMoves[setLegal] = { ...updatedLegalMoves[setLegal], isLegal: true };
+                            const setLegal = props.squares.findIndex(obj => obj.x === piece.x && obj.y === piece.y - i);
+                            if (props.squares[setLegal].isEmpty){
+                                updatedLegalMoves[setLegal] = { ...updatedLegalMoves[setLegal], isLegal: true };
+                                endFlag = false;
+                            }
+                            if (endFlag === true)
+                                break;
                         }
                     } else {
                         const setLegal = props.squares.findIndex(obj => obj.x === piece.x && obj.y === piece.y - 1 && obj.isEmpty === true);
@@ -87,6 +99,80 @@ function Board (props) {
                         updatedLegalMoves[attackRight] = { ...updatedLegalMoves[attackRight], isLegal: true };
                     }
                 }
+                break;
+
+            case 'rook':
+                const maxUp = 8 - piece.y + 1;
+                const maxDown = 8 - maxUp;
+                const maxRight = 8 - piece.x + 1;
+                const maxLeft = 8 - maxRight;
+
+                for (let i = 1 ; i < maxUp ; i++ ) {
+                    let endFlag = true;
+                    const up = props.squares.findIndex((obj) => obj.x === piece.x && obj.y === piece.y + i);
+                    if (props.squares[up].isEmpty) {
+                        updatedLegalMoves[up] = {...updatedLegalMoves[up], isLegal: true};
+                        endFlag = false;
+                    } else {
+                        if (color !== props.squares[up].type.split('_')[1]) {
+                            updatedLegalMoves[up] = {...updatedLegalMoves[up], isLegal: true};
+                        }
+                    }
+                    if (endFlag === true)
+                        break;
+                }
+                for (let i = 1 ; i < maxDown ; i++ ){
+                    let endFlag = true;
+                    const down = props.squares.findIndex((obj) => obj.x === piece.x && obj.y === piece.y - i);
+                    if (props.squares[down].isEmpty){
+                        updatedLegalMoves[down] = { ...updatedLegalMoves[down], isLegal: true };
+                        endFlag = false;
+                    }
+                    else {
+                        if (color !== props.squares[down].type.split('_')[1]){
+                            updatedLegalMoves[down] = { ...updatedLegalMoves[down], isLegal: true };
+                        }
+                    }
+                    if (endFlag === true)
+                        break;
+                }
+
+                for (let i = 1 ; i < maxRight ; i++ ){
+                    let endFlag = true;
+                    const right = props.squares.findIndex((obj) => obj.x === piece.x + i && obj.y === piece.y);
+                    if (props.squares[right].isEmpty){
+                        updatedLegalMoves[right] = { ...updatedLegalMoves[right], isLegal: true };
+                        endFlag = false;
+                    }
+                    else {
+                        if (color !== props.squares[right].type.split('_')[1]){
+                            updatedLegalMoves[right] = { ...updatedLegalMoves[right], isLegal: true };
+                        }
+                    }
+                    if (endFlag === true)
+                        break;
+                }
+
+                for (let i = 1 ; i < maxLeft ; i++ ){
+                    let endFlag = true;
+                    const left = props.squares.findIndex((obj) => obj.x === piece.x - i && obj.y === piece.y);
+                    if (props.squares[left].isEmpty){
+                        updatedLegalMoves[left] = { ...updatedLegalMoves[left], isLegal: true };
+                        endFlag = false;
+                    }
+                    else {
+                        if (color !== props.squares[left].type.split('_')[1]){
+                            updatedLegalMoves[left] = { ...updatedLegalMoves[left], isLegal: true };
+                        }
+                    }
+                    if (endFlag === true)
+                        break;
+                }
+
+
+
+
+
                 break;
             // Cases za ostanatite choechinja
 
